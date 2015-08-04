a7ce9e36b631b296c6163a98bf0b60c3563d81ff
PH111P@Arch <philipwellnitz@gmx.de>
2015-08-04 20:47:45 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='appoints',
+        version='1.0a1',
+        description='A Library for managing appointments',
+        author='Philip Wellnitz',
+        author_email='philipwellnitz@gmx.de',
+        packages=['appoints']
+    )
