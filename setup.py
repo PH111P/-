748a1c60eed9285282a24e63475a144d371331e9
PH111P@Arch <philipwellnitz@gmx.de>
2015-08-06 12:00:04 +0200

    ..

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='appoints',
-        version='1.1a',
+        version='1.1a1',
         description='A Library for managing appointments',
         author='Philip Wellnitz',
         author_email='philipwellnitz@gmx.de',
         url='https://github.com/PH111P/appoints',
-        packages=['appoints']
+        packages=['appoints'],
+        extras_require={
+            'Twofish encryption': ['twofish']
+        }
     )
